3ab56fb0ff84bf184091b1fb36629297cddb6ea1
berenkivraksal <151140213+berenkivraksal@users.noreply.github.com>
2023-11-21 20:58:09 +0300

    Update ceng105_github_assignment.c

--- ceng105_github_assignment.c
+++ ceng105_github_assignment.c
@@ -2,21 +2,13 @@
 int main (){
     int num1;
     int num2;
-    printf("Enter the first number:");
+    printf("Enter the first number: ");
     scanf("%d" , &num1);
-     printf("Enter the second number:");
+     printf("Enter the second number: ");
 scanf("%d" , &num2);
 printf("yes");
 
     return 0;
-
-
-
-
-
-
-
-
 
 
 }
